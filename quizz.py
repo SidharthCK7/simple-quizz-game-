c0d31325f60344cc394cfa0c5bb5b8524eeca9b6
sidharth ck <158825070+SidharthCK7@users.noreply.github.com>
2024-05-13 14:27:25 +0530

    Add files via upload

---
+++ quizz.py
@@ -0,0 +1,48 @@
+guess = int(input("enter the question number :- "))
+
+if guess == 1:
+    print("capital of india")
+    ans = "delhi"
+    x = input("enter the answer :- ")
+    if ans == x:
+        print(" correct answer ")
+    else:
+        print("wrong answer ")
+elif  guess ==2:
+    print(" prime minister of india")
+    ans ="narendra modhi"
+    x = input("enter the answer :- ")
+    if ans == x:
+        print(" correct answer ")
+    else:
+        print("wrong answer ")
+elif guess ==3:
+    print("national bird of india ")
+    ans="peacock"
+    x = input("enter the answer :- ")
+    if ans == x:
+        print(" correct answer ")
+    else:
+        print("wrong answer ")
+elif guess == 4:
+    print("who was the founder of  iphone")
+    ans="steve jobs "
+    x = input("enter the answer :- ")
+    if ans == x:
+        print(" correct answer ")
+    else:
+        print("wrong answer ")
+elif guess == 5:
+    print("who was the founder of the microsoft")
+    ans="bill gates"
+    x = input("enter the answer :- ")
+    if ans == x:
+        print(" correct answer ")
+    else:
+        print("wrong answer ")
+
+
+
+
+
+
